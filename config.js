8e49f68756107178e7cd461f0eabb7db61017ab4
Subrata Sarkar <tccdcc8@gmail.com>
2024-07-29 23:09:24 +0600

    m

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    jwt_secret: "amitomakevalobashi",
+    EMAIL: "tccdcc8@gmail.com",
+    PASS: "yfgaapsykhhhegdv",
+    ATLAS_URI:"mongodb+srv://AuthAppUser:AuthAppUser@authapp.obpckdc.mongodb.net/?retryWrites=true&w=majority"
+}
--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import ENV from '../config.js'
 
 async function connect(){
     const mongod =await MongoMemoryServer.create()
     const geturi= mongod.getUri()
     mongoose.set('strictQuery', true);
-    const db = await mongoose.connect(geturi)
+    const db = await mongoose.connect(ENV.ATLAS_URI)
     console.log("conneced to Database")
     return db
 }
